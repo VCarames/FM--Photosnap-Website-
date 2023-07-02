aff8d3d5f5f04515bbabef9d031ea1f3ef04898d
Valentine-D3V <109115310+VCarames@users.noreply.github.com>
2023-07-01 17:45:28 -0700

    Toggle Function [JS]

--- js/index.js
+++ js/index.js
@@ -36,3 +36,47 @@ navToggle.addEventListener("click", toggleNav);
 document.addEventListener("keydown", closeNavOnEscape);
 document.addEventListener("click", closeNavOnClickOutside);
 navMenu.addEventListener("focusout", closeNavOnFocusOut);
+
+const checkbox = document.querySelector(".toggle__checkbox");
+const priceMonth = document.querySelectorAll(".price__monthly");
+const priceYear = document.querySelectorAll(".price__yearly");
+
+const monthlyText = document.querySelectorAll(".monthly__text");
+const yearlyText = document.querySelectorAll(".yearly__text");
+
+// Check the value in localStorage and update the checkbox accordingly
+const savedCheckboxState = localStorage.getItem("checkboxState");
+if (savedCheckboxState === "true") {
+  checkbox.checked = true;
+  showYearlyPrices();
+} else {
+  checkbox.checked = false;
+  showMonthlyPrices();
+}
+
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    showYearlyPrices();
+  } else {
+    showMonthlyPrices();
+  }
+
+  // Store the state of the checkbox in localStorage
+  localStorage.setItem("checkboxState", checkbox.checked);
+});
+
+function showYearlyPrices() {
+  priceYear.forEach((price) => price.classList.remove("hidden"));
+  yearlyText.forEach((price) => price.classList.remove("hidden"));
+
+  priceMonth.forEach((price) => price.classList.add("hidden"));
+  monthlyText.forEach((price) => price.classList.add("hidden"));
+}
+
+function showMonthlyPrices() {
+  priceYear.forEach((price) => price.classList.add("hidden"));
+  yearlyText.forEach((price) => price.classList.add("hidden"));
+
+  priceMonth.forEach((price) => price.classList.remove("hidden"));
+  monthlyText.forEach((price) => price.classList.remove("hidden"));
+}
